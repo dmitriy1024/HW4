8e05bfcd699840ef9e60bbb7c9ca4233e29d3d0b
dmitriy1024 <dfedoryshyn@gmail.com>
2015-08-30 18:48:27 +0300

    Real sort was added

--- HW4.StaticClasses.Task2/Program.cs
+++ HW4.StaticClasses.Task2/Program.cs
@@ -9,7 +9,7 @@ namespace HW4.StaticClasses.Task2
             int[] array = { 3, 5, 7, 1, 2, 10, 9 };
             Console.WriteLine("Original array: ");
             foreach (var item in array)
-                Console.Write(" {0}  ",item);
+                Console.Write(" {0}  ", item);
 
             array.SortInAscending();
 
@@ -23,9 +23,21 @@ namespace HW4.StaticClasses.Task2
 
     static class IntArrExt
     {
-        public static void SortInAscending(this int [] arr)
-        {              
-            Array.Sort(arr);
+        public static void SortInAscending(this int[] arr)
+        {
+            int tmp;
+            int minIndex;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                minIndex = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                    if (arr[j] < arr[minIndex])
+                        minIndex = j;
+
+                tmp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = tmp;
+            }
         }
     }
 }
